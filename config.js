5b1636ccd9c9292e22f7d6af4db3d955615ebeeb
Rritvij <67905019+Rritvij@users.noreply.github.com>
2021-05-01 17:22:45 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,18 @@
+  import firebase from 'firebase'
+  require('@firebase/firestore')
+  
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBxW3mg-ZTZm89chy5ETrLvRMn9JbIZ94E",
+    authDomain: "story-hub-bcbeb.firebaseapp.com",
+    projectId: "story-hub-bcbeb",
+    storageBucket: "story-hub-bcbeb.appspot.com",
+    messagingSenderId: "588004467220",
+    appId: "1:588004467220:web:89f549b5daea86ae15fd01"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+ 
